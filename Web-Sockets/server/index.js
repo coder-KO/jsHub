afc87a7284f25854a3be32379f54c4040adcb539
Krishna <drkaojha@gmail.com>
2020-07-01 20:20:56 +0530

    adding string transfer b/w server and client

--- Web-Sockets/server/index.js
+++ Web-Sockets/server/index.js
@@ -8,6 +8,12 @@ const wss = new WebSocket.Server({ port: 8082 });
 wss.on("connection", ws =>{
     console.log("New Client Connected.!")
 
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+
+        ws.send(data.toUpperCase());
+    });
+
     ws.on("close", () => {
         console.log("Client has disconnected.!");
     });
